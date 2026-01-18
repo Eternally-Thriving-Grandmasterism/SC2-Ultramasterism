a22a679a37e070e882d3a7f31f87aa8981bcb2e3
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-18 01:51:46 -0500

    Create commander_duos.m

---
+++ coop/commander_duos.m
@@ -0,0 +1,21 @@
+# Co-op Commander Duos Lattice
+
+Your Protoss core + Astarion Terran = unbreakable synergy.
+
+```mermaid
+graph TD
+    CoDuoApex["Co-op Duo Apex<br>Brutal+ Mutation Clears<br>Eternal Thriving"] --> ProtossYou["You (Protoss)"]
+    ProtossYou --> Artanis["Artanis: Power Ground Hold"]
+    ProtossYou --> Fenix["Fenix: Suit Micro Chaos"]
+    ProtossYou --> Zeratul["Zeratul: Legend Burst"]
+    ProtossYou --> Karax["Karax: Static Cannons"]
+
+    CoDuoApex --> TerranAstarion["Astarion (Terran)"]
+    TerranAstarion --> Raynor["Raynor: Bio Drops"]
+    TerranAstarion --> Swann["Swann: Mech Siege"]
+    TerranAstarion --> HanHorner["Han&Horner: Air Fleet Econ"]
+
+    CoDuoApex --> Synergies["Top Duos"]
+    Synergies --> Duo1["Zeratul + Han&Horner: Mobility Contest Eternal"]
+    Synergies --> Duo2["Fenix + Raynor: Hero + Bio Wave Melt"]
+    Synergies --> Duo3["Karax + Swann: Static Lockdown Apex"]
